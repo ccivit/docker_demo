5fe4f68d781807334f59e1d1cbb9da5f3f12d96f
ccivit <carlescivit@gmail.com>
2018-08-29 18:00:15 -0700

    Added example including numpy library
    Example is similar to hello world, but now including importing of a library

---
+++ 1_script_w_numpy/numpy_script.py
@@ -0,0 +1,8 @@
+try:
+    import numpy as np
+    print('Successfully loaded numpy')
+    a=np.array([1,2,3])
+    print('Sample numpy array: ',a)
+except:
+    print('Could not load numpy. Check if it properly installed')
+
