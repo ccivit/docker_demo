4b459016b0b9ac3d8b15ae9cbd044b5196016964
ccivit <carlescivit@gmail.com>
2018-08-29 18:10:58 -0700

    Fixed spelling in error printout

--- 1_script_w_numpy/numpy_script.py
+++ 1_script_w_numpy/numpy_script.py
@@ -4,5 +4,5 @@ try:
     a=np.array([1,2,3])
     print('Sample numpy array: ',a)
 except:
-    print('Could not load numpy. Check if it properly installed')
+    print('Could not load numpy. Check if it is properly installed')
 
